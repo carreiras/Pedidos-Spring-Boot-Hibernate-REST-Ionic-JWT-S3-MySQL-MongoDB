078611135640a6c66a07f4563dd428ad38bfa4f3
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-11-21 13:07:14 -0300

    Criando repository e service para Categoria

--- src/main/java/com/diretoaocodigo/pedidos/rest/controller/CategoriaController.java
+++ src/main/java/com/diretoaocodigo/pedidos/rest/controller/CategoriaController.java
@@ -1,26 +1,24 @@
 package com.diretoaocodigo.pedidos.rest.controller;
 
 import com.diretoaocodigo.pedidos.domain.entity.Categoria;
+import com.diretoaocodigo.pedidos.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Categoria> listar() {
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
+    @Autowired
+    private CategoriaService categoriaService;
 
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-
-        return lista;
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> listar(@PathVariable Integer id) {
+        Categoria categoria = categoriaService.buscar(id);
+        return ResponseEntity.ok().body(categoria);
     }
 }
---
+++ src/main/java/com/diretoaocodigo/pedidos/service/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.diretoaocodigo.pedidos.service;
+
+import com.diretoaocodigo.pedidos.domain.entity.Categoria;
+import com.diretoaocodigo.pedidos.domain.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        return categoria.orElse(null);
+    }
+}
