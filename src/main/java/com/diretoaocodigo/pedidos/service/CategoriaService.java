de7d76f78c3e1f26a8c23882f67c5dfcde25d827
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-11-21 14:25:13 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/diretoaocodigo/pedidos/service/CategoriaService.java
+++ src/main/java/com/diretoaocodigo/pedidos/service/CategoriaService.java
@@ -2,6 +2,7 @@ package com.diretoaocodigo.pedidos.service;
 
 import com.diretoaocodigo.pedidos.domain.entity.Categoria;
 import com.diretoaocodigo.pedidos.domain.repository.CategoriaRepository;
+import com.diretoaocodigo.pedidos.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class CategoriaService {
 
     public Categoria findById(Integer id) {
         Optional<Categoria> categoria = categoriaRepository.findById(id);
-        return categoria.orElse(null);
+        return categoria.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
