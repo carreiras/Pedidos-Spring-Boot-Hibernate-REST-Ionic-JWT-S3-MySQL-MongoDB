3727f5c2084f0387d1c762f5f4ffc6224d7fa082
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-12-07 10:54:27 -0300

    Estado e cidade

---
+++ src/main/java/com/diretoaocodigo/pedidos/domain/repository/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.diretoaocodigo.pedidos.domain.repository;
+
+import com.diretoaocodigo.pedidos.domain.entity.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+}
