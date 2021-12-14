8ae870f2ff997c6f6f9b383b2b9cc16f9862848f
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-12-14 11:05:33 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/diretoaocodigo/pedidos/rest/controller/CategoriaController.java
+++ src/main/java/com/diretoaocodigo/pedidos/rest/controller/CategoriaController.java
@@ -4,10 +4,10 @@ import com.diretoaocodigo.pedidos.domain.entity.Categoria;
 import com.diretoaocodigo.pedidos.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -20,5 +20,12 @@ public class CategoriaController {
     public ResponseEntity<?> findById(@PathVariable Integer id) {
         Categoria categoria = categoriaService.findById(id);
         return ResponseEntity.ok().body(categoria);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+        Categoria saved = categoriaService.insert(categoria);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(saved.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
