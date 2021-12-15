8c5f8dc83884d7595a2e85a0b6cf5fd8df35a84a
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-12-15 12:58:46 -0300

    Personalizando o retorno da validação

--- src/main/java/com/diretoaocodigo/pedidos/rest/exception/RestExceptionHandler.java
+++ src/main/java/com/diretoaocodigo/pedidos/rest/exception/RestExceptionHandler.java
@@ -2,9 +2,10 @@ package com.diretoaocodigo.pedidos.rest.exception;
 
 import com.diretoaocodigo.pedidos.exception.DataIntegratyException;
 import com.diretoaocodigo.pedidos.exception.ObjectNotFoundException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,5 +24,14 @@ public class RestExceptionHandler {
     public ResponseEntity<StandardError> handlerDataIntegratyException(DataIntegratyException e, HttpServletRequest request) {
         StandardError standardError = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        ValidationError validationError = new ValidationError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        for (FieldError x : e.getBindingResult().getFieldErrors()) {
+            validationError.addError(x.getField(), x.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(validationError);
     }
 }
