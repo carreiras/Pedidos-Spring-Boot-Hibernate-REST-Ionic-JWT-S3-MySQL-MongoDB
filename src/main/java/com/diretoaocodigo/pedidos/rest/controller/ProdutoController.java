b36ee490437e2fdb3f39ff6c30b32b04aec5faaa
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-12-18 11:33:37 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/diretoaocodigo/pedidos/rest/controller/ProdutoController.java
+++ src/main/java/com/diretoaocodigo/pedidos/rest/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.diretoaocodigo.pedidos.rest.controller;
 import com.diretoaocodigo.pedidos.domain.entity.Categoria;
 import com.diretoaocodigo.pedidos.domain.entity.Pedido;
 import com.diretoaocodigo.pedidos.domain.entity.Produto;
+import com.diretoaocodigo.pedidos.rest.controller.utils.URL;
 import com.diretoaocodigo.pedidos.rest.dto.CategoriaDto;
 import com.diretoaocodigo.pedidos.rest.dto.ProdutoDto;
 import com.diretoaocodigo.pedidos.service.ProdutoService;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/produtos")
@@ -26,14 +29,16 @@ public class ProdutoController {
 
     @RequestMapping(value = "/page", method = RequestMethod.GET)
     public ResponseEntity<Page<ProdutoDto>> findPage(
-            @RequestParam(value = "nome", defaultValue = "") Integer nome,
-            @RequestParam(value = "categorias", defaultValue = "") Integer categorias,
+            @RequestParam(value = "nome", defaultValue = "") String nome,
+            @RequestParam(value = "categorias", defaultValue = "") String categorias,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
             @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
             @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
-        Page<Produto> produtoPage = produtoService.search(page, linesPerPage, orderBy, direction);
-        Page<CategoriaDto> categoriaPageDto = produtoPage.map(categoria -> new CategoriaDto(categoria));
-        return ResponseEntity.ok().body(categoriaPageDto);
+        String nomeDecoded = URL.decodeParam(nome);
+        List<Integer> ids = URL.decodeIntList(categorias);
+        Page<Produto> produtoPage = produtoService.search(nomeDecoded, ids, page, linesPerPage, orderBy, direction);
+        Page<ProdutoDto> produtoPageDto = produtoPage.map(produto -> new ProdutoDto(produto));
+        return ResponseEntity.ok().body(produtoPageDto);
     }
 }
