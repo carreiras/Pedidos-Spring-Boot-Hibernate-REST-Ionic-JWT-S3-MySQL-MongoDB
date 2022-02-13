e6d31b8ca8ca4f5265de434b66e59a548fe21961
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-02-13 17:46:54 -0300

    Refresh token

--- src/main/java/com/diretoaocodigo/pedidos/config/SecurityConfig.java
+++ src/main/java/com/diretoaocodigo/pedidos/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/categorias/**"
     };
     private static final String[] PUBLIC_MATCHERS_POST = {
-            "/clientes/**"
+            "/clientes/**",
+            "/auth/refresh_token/**"
     };
 
     @Autowired
--- src/main/java/com/diretoaocodigo/pedidos/service/UserService.java
+++ src/main/java/com/diretoaocodigo/pedidos/service/UserService.java
@@ -8,7 +8,8 @@ public class UserService {
     public static UserSS authenticated() {
         try {
             return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             return null;
         }
     }
