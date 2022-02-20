02700575db5cd2850b048990692fb7087d9c6d80
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-02-20 11:03:14 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/diretoaocodigo/pedidos/PedidosApiApplication.java
+++ src/main/java/com/diretoaocodigo/pedidos/PedidosApiApplication.java
@@ -1,5 +1,7 @@
 package com.diretoaocodigo.pedidos;
 
+import com.diretoaocodigo.pedidos.service.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PedidosApiApplication implements CommandLineRunner {
 
+    @Autowired
+    private S3Service s3Service;
+
     public static void main(String[] args) {
         SpringApplication.run(PedidosApiApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        s3Service.uploadFile("C:\\temp\\fotos\\bonitao.jpg");
     }
 }
