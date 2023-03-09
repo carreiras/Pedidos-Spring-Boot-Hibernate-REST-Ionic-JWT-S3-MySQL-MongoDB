5e4acc0bbd1d1d4109814d1976953909458a02c4
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-03-09 12:07:53 -0300

    Renomeando arquivo de mola

--- src/main/java/com/carreiras/pedidos/Application.java
+++ src/main/java/com/carreiras/pedidos/Application.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PedidosApplication implements CommandLineRunner {
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(PedidosApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     @Override
